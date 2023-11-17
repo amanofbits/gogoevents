52a5bb3df24023b8340b0864b5e8186cca67f3d4
amanofbits <7976963+amanofbits@users.noreply.github.com>
2023-11-17 16:12:49 +0200

    added missed header

--- internal/wildcard/wildcard_test.go
+++ internal/wildcard/wildcard_test.go
@@ -1,3 +1,23 @@
+/*
+ * Holds wildcard tests
+ *
+ * Original code copyright:
+ *
+ * Copyright (c) 2023 Iglou.eu <contact@iglou.eu>
+ * Copyright (c) 2023 Adrien Kara <adrien@iglou.eu>
+ *
+ * This file is part of gogoevents.
+ * modifications copyright:
+ * © 2023 amanofbits
+ *
+ * gogoevents is free software: you can redistribute it and/or modify
+ * it under the terms of the BSD 3-Clause License as published by the
+ * University of California. See the `LICENSE` file for more details.
+ *
+ * You should have received a copy of the BSD 3-Clause License along with
+ * gogoevents. If not, see <https://opensource.org/licenses/BSD-3-Clause>.
+ */
+
 package wildcard_test
 
 import (
